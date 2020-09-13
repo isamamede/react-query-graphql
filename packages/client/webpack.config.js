138fe228df85782f704769ef874caa301c4c901b
Isa Mamede <isamamede1@gmail.com>
2020-09-13 11:54:44 -0300

    inital commit

--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -1,6 +1,4 @@
 const path = require("path");
-const webpack = require("webpack");
-const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 const isDevelopment = false;
 
